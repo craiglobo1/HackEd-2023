66248f70b43c4fd42fed9c84a6303c898a87649e
craiglobo1 <craiglobo1@gmail.com>
2023-01-07 19:19:37 -0700

    added generate_typed notes route

--- main.py
+++ main.py
@@ -22,6 +22,40 @@ def detect_texts(uri):
     
     return response.full_text_annotation.pages[0]
 
+def get_ocr_data_from_uri(uri):
+    page = detect_texts(uri)
+
+    data ={
+        "blocks": []
+    }
+    for block in page.blocks:
+        c_block = {
+            "bounds" : [ (ver.x, ver.y) for ver in block.bounding_box.vertices],
+            "paras" : []
+        }
+
+        for paragraph in block.paragraphs:
+            c_para = {
+                "bounds" : [ (ver.x, ver.y) for ver in paragraph.bounding_box.vertices],
+                "words" : []
+            }
+            for word in paragraph.words:
+                c_word = {
+                    "bounds" : [ (ver.x, ver.y) for ver in word.bounding_box.vertices],
+                    "chars" : []
+                }
+                for symbol in word.symbols:
+                    c_symbol = {
+                        "bounds" : [ (ver.x, ver.y) for ver in symbol.bounding_box.vertices],
+                        "text" : symbol.text
+                    }
+                    c_word["chars"].append(c_symbol)
+                c_word["text"] = "".join([ s["text"] for s in c_word["chars"]])
+                c_para["words"].append(c_word)
+            c_para["text"] = " ".join([ s["text"] for s in c_para["words"]])
+            c_block["paras"].append(c_para)
+        data["blocks"].append(c_block)
+    return data
 
 
 @app.route("/")
@@ -35,44 +69,27 @@ def hello_world():
 @app.route("/v1/get_text_bounds/")
 def get_text_bounds():
     app.logger.info(request.args)
-    if request.args.get("img_path", default=-1) == -1 and request.args.get("uri", default=-1) == -1:
-        return jsonify({"Error" : "No img path provided"})
+    if request.args.get("uri", default=-1) == -1:
+        return jsonify({"Error" : "No uri for image path provided"})
     else:
-        page = detect_texts(request.args["img_path"])
-
-        data ={
-            "blocks": []
-        }
-        for block in page.blocks:
-            c_block = {
-                "bounds" : [ (ver.x, ver.y) for ver in block.bounding_box.vertices],
-                "paras" : []
-            }
-
-            for paragraph in block.paragraphs:
-                c_para = {
-                    "bounds" : [ (ver.x, ver.y) for ver in paragraph.bounding_box.vertices],
-                    "words" : []
-                }
-                for word in paragraph.words:
-                    c_word = {
-                        "bounds" : [ (ver.x, ver.y) for ver in word.bounding_box.vertices],
-                        "chars" : []
-                    }
-                    for symbol in word.symbols:
-                        c_symbol = {
-                            "bounds" : [ (ver.x, ver.y) for ver in symbol.bounding_box.vertices],
-                            "text" : symbol.text
-                        }
-                        c_word["chars"].append(c_symbol)
-                    c_word["text"] = "".join([ s["text"] for s in c_word["chars"]])
-                    c_para["words"].append(c_word)
-                c_para["text"] = " ".join([ s["text"] for s in c_para["words"]])
-                c_block["paras"].append(c_para)
-            data["blocks"].append(c_block)
+        data = get_ocr_data_from_uri(request.args["uri"])
 
         return jsonify(data)
 
+@app.route("/v1/generate_typed_notes/")
+def generate_typed_notes():
+    if request.args.get("uris", default=-1) == -1:
+        return jsonify({"Error" : "No img uris provided"})
+    else:
+        try:
+            uris = eval(request.args.get("uris"))
+            if isinstance(uris, list):
+                if all([ isinstance(val, str) for val in uris]):
+                    
+                    return jsonify(uris)
+        except:
+            pass
+        return jsonify({"Error" : "Invalid uris provided"})
 
 if __name__ == "__main__":
     app.run()
