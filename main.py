987dbf10815917487a249ae95eaf607d2da15a7a
craiglobo1 <craiglobo1@gmail.com>
2023-01-08 08:26:45 -0700

    added more heuristic for the top of the page and refactored the paragraphs

--- main.py
+++ main.py
@@ -27,47 +27,74 @@ class PDF(FPDF):
         self.ln(4)
 
 
-def extract_data(img_url):
-    req = requests.get(F"https://handwrite-374020.wn.r.appspot.com/v1/get_text_bounds/?uri={img_url}")
-    data = req.json()
+get_height = lambda x : max(x)-min(x)
 
-    get_height = lambda x : max(x)-min(x)
-
+def extract_data(data):
     heights = [get_height([ bound[1] for bound in para["bounds"]]) for block in data["blocks"] for para in block["paras"] ]
 
     avg_height = sum(heights)/len(heights)
 
-    paragraphs = [para["text"] for block in data["blocks"] for para in block["paras"] if get_height([bound[0] for bound in para["bounds"]]) >= avg_height*0.5]
-    paragraphs = [x for x in paragraphs if any(c.isalpha() for c in x)]
+    paragraphs = [para for block in data["blocks"] for para in block["paras"] if get_height([bound[0] for bound in para["bounds"]]) >= avg_height*0.5]
+    paragraphs = [para for para in paragraphs if any(char.isalpha() for char in para["text"])]
 
     for i in range(len(paragraphs)):
-        paragraphs[i] = paragraphs[i].replace(" .", ".")
-        paragraphs[i] = paragraphs[i].replace(". ", ".")
-        paragraphs[i] = paragraphs[i].replace(".", ". ")
-        paragraphs[i] = paragraphs[i].replace(" ,", ",")
-        paragraphs[i] = paragraphs[i].replace(", ", ",")
-        paragraphs[i] = paragraphs[i].replace(",", ", ")
-        paragraphs[i] = paragraphs[i].replace(" )", ")")
-        paragraphs[i] = paragraphs[i].replace(") ", ")")
-        paragraphs[i] = paragraphs[i].replace(")", ") ")
-        paragraphs[i] = paragraphs[i].replace(" (", "(")
-        paragraphs[i] = paragraphs[i].replace("( ", "(")
-        paragraphs[i] = paragraphs[i].replace("(", " (")
-        paragraphs[i] = paragraphs[i].replace(" -", "-")
-        paragraphs[i] = paragraphs[i].replace("- ", "-")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(" .", ".")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(". ", ".")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(".", ". ")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(" ,", ",")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(", ", ",")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(",", ", ")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(" )", ")")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(") ", ")")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(")", ") ")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(" (", "(")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace("( ", "(")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace("(", " (")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace(" -", "-")
+        paragraphs[i]["text"] = paragraphs[i]["text"].replace("- ", "-")
 
     return paragraphs
 
 
-
 def extractOrder(paragraphs):
+    for i, para in enumerate(paragraphs):
+        word_heights = [ get_height([ bound[1] for bound in word["bounds"]]) for word in para["words"]]
+        avg_word_height = sum(word_heights)/len(word_heights)
+        paragraphs[i]["fnt_size"] = avg_word_height
+    
+    fnt_sizes = [ para["fnt_size"] for para in paragraphs]
+    max_size = max(fnt_sizes)
+    min_size = min(fnt_sizes)
+    size_range = max_size-min_size
+
+
+    max_width = -1
+    max_height = -1
+    for i, para in enumerate(paragraphs):
+        max_width = max(max_width, max([ bound[0] for bound in para["bounds"]]))
+        max_height = max(max_height, max([ bound[1] for bound in para["bounds"]]))
+    
+    print(max_width, max_height)
+
     order = []
-    for i in range(len(paragraphs)):
-        # print([para for para in paragraphs[i].split(" ") if para != ""])
-        if all([ para[0].isupper() for para in paragraphs[i].split(" ") if para != ""]):
+    for i, para in enumerate(paragraphs):
+        paragraphs[i]["fnt_size"] = (paragraphs[i]["fnt_size"] - min_size)/size_range
+        if min([bounds[1] for bounds in para["bounds"]]) < max_height*0.1:
+            paragraphs[i]["fnt_size"] *= 1.1
+        if paragraphs[i]["fnt_size"] >= 0.95:
             order.append('h')
         else:
             order.append('p')
+    
+    print([ para["fnt_size"] for para in paragraphs])
+
+    # order = []
+    # for i in range(len(paragraphs)):
+    #     # print([para for para in paragraphs[i].split(" ") if para != ""])
+    #     if all([ para[0].isupper() for para in paragraphs[i].split(" ") if para != ""]):
+    #         order.append('h')
+    #     else:
+    #         order.append('p')
     return order
 
 def pages_to_pdf(uris):
@@ -77,15 +104,15 @@ def pages_to_pdf(uris):
     wpdf.add_font('Roboto', '', 'Roboto-Regular.ttf')
     for uri in uris:
         uri = unquote(uri)
-        paragraphs = extract_data(uri)
+        page_data = requests.get(F"https://handwrite-374020.wn.r.appspot.com/v1/get_text_bounds/?uri={uri}").json()
+        paragraphs = extract_data(page_data)
         order = extractOrder(paragraphs)
-
 
         for i in range(len(order)):
             if order[i] == 'h':
-                wpdf.headline(paragraphs[i])
+                wpdf.headline(paragraphs[i]["text"])
             elif order[i] == 'p':
-                wpdf.paragraph(paragraphs[i])
+                wpdf.paragraph(paragraphs[i]["text"])
 
     return wpdf.output(dest='S')
 
