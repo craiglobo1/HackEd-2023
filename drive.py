6d4a78f5e019a38b2f242628822b086605f496a1
craiglobo1 <craiglobo1@gmail.com>
2023-01-07 23:26:34 -0700

    added route /v1/get_text_bounds/ but only works on latin-1 encoding

---
+++ drive.py
@@ -0,0 +1,55 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+
+def upload_to_drive(filename):
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+
+        file_metadata = {'name': filename}
+        media = MediaFileUpload(filename,
+                                mimetype='application/pdf')
+        # pylint: disable=maybe-no-member
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        # print(F'File ID: {file.get("id")}')
+        return file.get("id")
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+
+
+if __name__ == '__main__':
+    print(upload_to_drive("pg1.jpg"))
--- main.py
+++ main.py
@@ -1,8 +1,85 @@
 from flask import Flask, jsonify, request
 from urllib.parse import unquote
 from google.cloud import vision
+import requests
+from fpdf import FPDF
+from drive import upload_to_drive
 
 app = Flask(__name__)
+
+class PDF(FPDF):
+    def headline(self, headline):
+        self.set_font('Times', 'B', 18)
+        self.set_fill_color(255, 255, 255)
+        self.cell(0, 5, headline, 0, 1, 'C', 1)
+        self.ln(4)
+
+    def paragraph(self, paragraph):
+        self.set_font('Times', '', 14)
+        self.set_fill_color(255, 255, 255)
+        self.multi_cell(0, 5, paragraph)
+        self.ln(4)
+
+
+def extract_data(img_url):
+    req = requests.get(F"https://handwrite-374020.wn.r.appspot.com/v1/get_text_bounds/?uri={img_url}")
+    data = req.json()
+
+    get_height = lambda x : max(x)-min(x)
+
+    heights = [get_height([ bound[1] for bound in para["bounds"]]) for block in data["blocks"] for para in block["paras"] ]
+
+    avg_height = sum(heights)/len(heights)
+
+    paragraphs = [para["text"] for block in data["blocks"] for para in block["paras"] if get_height([bound[0] for bound in para["bounds"]]) >= avg_height*0.5]
+    paragraphs = [x for x in paragraphs if any(c.isalpha() for c in x)]
+
+    for i in range(len(paragraphs)):
+        paragraphs[i] = paragraphs[i].replace(" .", ".")
+        paragraphs[i] = paragraphs[i].replace(". ", ".")
+        paragraphs[i] = paragraphs[i].replace(".", ". ")
+        paragraphs[i] = paragraphs[i].replace(" ,", ",")
+        paragraphs[i] = paragraphs[i].replace(", ", ",")
+        paragraphs[i] = paragraphs[i].replace(",", ", ")
+        paragraphs[i] = paragraphs[i].replace(" )", ")")
+        paragraphs[i] = paragraphs[i].replace(") ", ")")
+        paragraphs[i] = paragraphs[i].replace(")", ") ")
+        paragraphs[i] = paragraphs[i].replace(" (", "(")
+        paragraphs[i] = paragraphs[i].replace("( ", "(")
+        paragraphs[i] = paragraphs[i].replace("(", " (")
+        paragraphs[i] = paragraphs[i].replace(" -", "-")
+        paragraphs[i] = paragraphs[i].replace("- ", "-")
+
+    return paragraphs
+
+
+
+def extractOrder(paragraphs):
+    order = []
+    for i in range(len(paragraphs)):
+        # print([para for para in paragraphs[i].split(" ") if para != ""])
+        if all([ para[0].isupper() for para in paragraphs[i].split(" ") if para != ""]):
+            order.append('h')
+        else:
+            order.append('p')
+    return order
+
+def pages_to_pdf(uris, output_filename="formatted.pdf"):
+    wpdf = PDF()
+    wpdf.add_page()    
+    for uri in uris:
+        uri = unquote(uri)
+        paragraphs = extract_data(uri)
+        order = extractOrder(paragraphs)
+
+
+        for i in range(len(order)):
+            if order[i] == 'h':
+                wpdf.headline(paragraphs[i])
+            elif order[i] == 'p':
+                wpdf.paragraph(paragraphs[i])
+
+    wpdf.output(output_filename, 'F')
 
 def detect_texts(uri):
     """Detects text in the file."""
@@ -83,13 +160,18 @@ def generate_typed_notes():
     else:
         try:
             uris = eval(request.args.get("uris"))
-            if isinstance(uris, list):
-                if all([ isinstance(val, str) for val in uris]):
-                    
-                    return jsonify(uris)
         except:
-            pass
-        return jsonify({"Error" : "Invalid uris provided"})
+            return jsonify({"Error" : "Invalid uris provided"})
+            
+        if isinstance(uris, list):
+            # if all([ isinstance(val, str) for val in uris]):
+            app.logger.info("created pdf")
+            pages_to_pdf(uris)
+            upload_to_drive("formatted.pdf")
+
+            return jsonify(uris)
+        else:
+            return jsonify({"Error" : "uris not a list"})
 
 if __name__ == "__main__":
     app.run()
